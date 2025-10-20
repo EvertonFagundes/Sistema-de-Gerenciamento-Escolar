7ced03d1aec96d1315a18f2f9da39054fa22b958
Everton Fagundes <evertonfagundes1510@gmail.com>
2025-10-20 16:31:17 -0300

    Adiciona classe Professor e classe Disciplina

--- src/modelo/Disciplina.java
+++ src/modelo/Disciplina.java
@@ -1,5 +1,48 @@
 package modelo;
 
 public class Disciplina {
-    
+    String nome;
+    String codigo;
+    String descricao;
+    int cargaHoraria;
+
+    public Disciplina(String nome, String codigo, String descricao, int cargaHoraria){
+        this.nome = nome;
+        this.codigo = codigo;
+        this.descricao = descricao;
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
 }
--- src/modelo/Professor.java
+++ src/modelo/Professor.java
@@ -1,5 +1,31 @@
 package modelo;
 
-public class Professor {
+import java.util.ArrayList;
+
+public class Professor extends Usuario{
+    String formacaoAcademica;
+    ArrayList<Disciplina>disciplinasLeciona = new ArrayList<>();
+
+    public Professor(String nome, String cpf, String rg, String matricula, String email, int diaNasc, int mesNasc, int anoNasc,String nomeRua, String nomeBairro, String nomeCidade, int numeroCasa, String complemento, int numeroTelefone, String formacaoAcademica, ArrayList<Disciplina>disciplinasLeciona){
+        super(nome, cpf, rg, matricula, email, diaNasc, mesNasc, anoNasc, nomeRua, nomeBairro, nomeCidade, numeroCasa, complemento, numeroTelefone);
+        this.formacaoAcademica = formacaoAcademica;
+        this.disciplinasLeciona = disciplinasLeciona;
+    }
+
+    public String getFormacaoAcademica() {
+        return formacaoAcademica;
+    }
+
+    public void setFormacaoAcademica(String formacaoAcademica) {
+        this.formacaoAcademica = formacaoAcademica;
+    }
+
+    public ArrayList<Disciplina> getDisciplinasLeciona() {
+        return disciplinasLeciona;
+    }
+
+    public void setDisciplinasLeciona(ArrayList<Disciplina> disciplinasLeciona) {
+        this.disciplinasLeciona = disciplinasLeciona;
+    }
     
 }
